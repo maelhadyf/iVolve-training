40a6787251b30667b28782c2b6f13c6494057903
maelhadyf <maelhadyf@gmail.com>
2024-12-09 10:21:30 +0200

    Create variables.tf

---
+++ Terraform/lab_17/variables.tf
@@ -0,0 +1,12 @@
+# variables.tf
+variable "db_username" {
+  description = "Database administrator username"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "Database administrator password"
+  type        = string
+  sensitive   = true
+}
